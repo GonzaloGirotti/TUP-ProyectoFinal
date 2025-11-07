e089518ea01cf8964c321477a0432ce59719df46
marcos-1011 <marcos.diaz3088@gmail.com>
2025-11-07 16:03:32 -0300

    probando automatizacion con husky

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,17 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import app from "./app";
-import sequelize from "./config/db";
+import app from './app';
+import sequelize from './config/db';
 
 const PORT = process.env.PORT || 4000;
 
 (async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("Base de datos conectada correctamente");
-        app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
-    } catch (error) {
-        console.error('Error al conectar con la BD:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('Base de datos conectada correctamente');
+    app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
+  } catch (error) {
+    console.error('Error al conectar con la BD:', error);
+  }
 })();
